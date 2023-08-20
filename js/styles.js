bd8446f01e29e309e5c8782fe6fe16a701b56d37
Sniper Ghost <gsniper520@gmail.com>
2023-08-21 05:12:06 +0600

    added a new section

---
+++ js/styles.js
@@ -0,0 +1,100 @@
+// <!-- 1st card-->
+function handleClickButton (){
+    // <!-- 1st card text section-->
+    const selectedItemsContainer = document.getElementById('selected-items');
+    const handleClickId = document.getElementById('handle-text-id1');
+    const handleClickText = handleClickId.innerText;
+    const li = document.createElement("li");
+    li.innerText = handleClickText;
+    selectedItemsContainer.appendChild(li);
+
+    // <!-- 1st card price section-->
+    const handlePriceId = document.getElementById('handle-price-id1');
+    const handelPriceText = handlePriceId.innerText;
+    console.log(handelPriceText);
+}
+
+// <!-- 2nd card-->
+function handleClickButton2 (){
+    // <!-- 2nd card text section-->
+    const selectedItemsContainer = document.getElementById('selected-items');
+    const handleClickId2 = document.getElementById('handle-text-id2');
+    const handleClickText2 = handleClickId2.innerText;
+    const li = document.createElement("li");
+    li.innerText = handleClickText2;
+    selectedItemsContainer.appendChild(li);
+
+    // <!-- 2nd card price section-->
+    const handlePriceId2 = document.getElementById('handle-price-id2');
+    const handelPriceText2 = handlePriceId2.innerText;
+    console.log(handelPriceText2);
+}
+
+
+// <!-- 3rd card-->
+function handleClickButton3 (){
+    // <!-- 3rd card text section-->
+    const selectedItemsContainer = document.getElementById('selected-items');
+    const handleClickId3 = document.getElementById('handle-text-id3');
+    const handleClickText3 = handleClickId3.innerText;
+    const li = document.createElement("li");
+    li.innerText = handleClickText3;
+    selectedItemsContainer.appendChild(li);
+
+    // <!-- 3rd card price section-->
+    const handlePriceId3 = document.getElementById('handle-price-id3');
+    const handelPriceText3 = handlePriceId3.innerText;
+    console.log(handelPriceText3);
+}
+
+
+// <!-- 4th card-->
+function handleClickButton4 (){
+    // <!-- 4th card text section-->
+    const selectedItemsContainer = document.getElementById('selected-items');
+    const handleClickId4 = document.getElementById('handle-text-id4');
+    const handleClickText4 = handleClickId4.innerText;
+    const li = document.createElement("li");
+    li.innerText = handleClickText4;
+    selectedItemsContainer.appendChild(li);
+
+    // <!-- 4th card price section-->
+    const handlePriceId4 = document.getElementById('handle-price-id4');
+    const handelPriceText4 = handlePriceId4.innerText;
+    console.log(handelPriceText4);
+}
+
+
+// <!-- 5th card-->
+function handleClickButton5 (){
+    // <!-- 5th card text section-->
+    const selectedItemsContainer = document.getElementById('selected-items');
+    const handleClickId5 = document.getElementById('handle-text-id5');
+    const handleClickText5 = handleClickId5.innerText;
+    const li = document.createElement("li");
+    li.innerText = handleClickText5;
+    selectedItemsContainer.appendChild(li);
+
+    // <!-- 5th card price section-->
+    const handlePriceId5 = document.getElementById('handle-price-id5');
+    const handelPriceText5 = handlePriceId5.innerText;
+    console.log(handelPriceText5);
+}
+
+
+// <!-- 6th card-->
+function handleClickButton6 (){
+    // <!-- 6th card text section-->
+    const selectedItemsContainer = document.getElementById('selected-items');
+    const handleClickId6 = document.getElementById('handle-text-id6');
+    const handleClickText6 = handleClickId6.innerText;
+    const li = document.createElement("li");
+    li.innerText = handleClickText6;
+    selectedItemsContainer.appendChild(li);
+
+    // <!-- 6th card price section-->
+    const handlePriceId6 = document.getElementById('handle-price-id6');
+    const handelPriceText6 = handlePriceId6.innerText;
+    console.log(handelPriceText6);
+}
+
